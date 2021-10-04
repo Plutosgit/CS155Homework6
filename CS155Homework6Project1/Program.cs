27dd41926f607431d5e364bd866ff178e4b4ecda
Plutosgit <venkataraman_balaji@hotmail.com>
2021-10-04 15:36:50 -0700

    Add project files.

---
+++ CS155Homework6Project1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS155Homework6Project1
+{
+    class Program
+    {
+        public const int SIZE = 10;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello! homework 6! Part 1");
+            Console.WriteLine("-------------------------");
+
+            //char[,] myArray1 = new char[SIZE, SIZE]
+            //    {
+            //    };
+
+            string[] myArray1 = new string[SIZE];
+
+            myArray1[0] = "Mira Costa";
+            myArray1[1] = "Palomar";
+            myArray1[2] = "SD Mesa";
+            myArray1[3] = "Miramar";
+            myArray1[4] = "CSU-San Marcos";
+            myArray1[5] = "San Diego State";
+            myArray1[6] = "UCSD San Diego";
+            myArray1[7] = "UC Irvine";
+            myArray1[8] = "UC Los Angeles";
+            myArray1[9] = "UC Berkeley";
+
+            Console.WriteLine(myArray1.Length.ToString());
+            string[] myArray2 = new string[myArray1.Length];
+
+            //return;
+
+            for (int i = 0; i < SIZE; i++)
+                myArray2[i] = myArray1[i];  // String.Copy(myArray1[i]);  //copy array1 to array2
+
+            //now print
+            //
+            Console.WriteLine("       -Array1-");
+            Console.WriteLine("  Index       Value  ");
+            for (int i = 0; i < SIZE; i++)
+                Console.WriteLine("   " + i.ToString() + "      " + myArray1[i]);
+
+            Console.WriteLine();
+
+            Console.WriteLine("       -Array2 (copied)-");
+            Console.WriteLine("  Index       Value  ");
+            for (int i = 0; i < SIZE; i++)
+                Console.WriteLine("   " + i.ToString() + "      " + myArray2[i]);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CS155Homework6Project2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS155Homework6Project2
+{
+    class Program
+    {
+        public const int SIZE = 10;
+        public const int RND_MIN = 0;
+        public const int RND_MAX = 1000;
+
+        static void Reverse(int[] Nums)
+        {
+            int temp;
+
+            for (int i=0; i < SIZE/2; i++)   //loop only until half the array size for swapping..
+            {
+                temp = Nums[i];
+                Nums[i] = Nums[SIZE - i - 1];
+                Nums[SIZE - i - 1] = temp;
+            }
+
+        }
+
+        static int[] GenerateNumbers()
+        {
+            int[] pArr = new int[SIZE];  // return the array
+
+            Random rndA = new Random();
+            for (int i = 0; i < SIZE; i++)
+                pArr[i] = rndA.Next(RND_MIN, RND_MAX);
+
+            return pArr;
+        }
+
+        static void PrintNumbers(int[] Nums, int[] NumsOrig)
+        {
+            //now print
+            //
+            Console.WriteLine("       -Array (Original)-");
+            Console.WriteLine("Index   Value  ");
+            for (int i = 0; i < SIZE; i++)
+                Console.WriteLine("   " + i.ToString() + "      " + NumsOrig[i]);
+
+            Console.WriteLine();
+
+            Console.WriteLine("       -Array (Reversed)-");
+            Console.WriteLine("Index   Value  ");
+            for (int i = 0; i < SIZE; i++)
+                Console.WriteLine("   " + i.ToString() + "      " + Nums[i]);
+
+            Console.ReadLine();
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Homework 6 - Project 2; Reversing an array");
+            Console.WriteLine("-------------------------------------------------");
+
+            int[] numbersOriginal = GenerateNumbers();
+            int[] numbers = GenerateNumbers();  // we need to initialize the array, but we don't need the new numbers
+
+            //Copy the original numbers to numbers before reversing them
+            for (int i = 0; i < SIZE; i++)
+                numbers[i] = numbersOriginal[i];
+
+            Reverse(numbers);
+            PrintNumbers(numbers, numbersOriginal);
+        }
+    }
+}
